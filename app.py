3ebc81615d0d25ef4ea3ddf7748813fe414dec2c
Azao-dev <azanhg02@gmail.com>
2025-11-03 21:55:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,5 @@ def image_to_text():
                 return e
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    app.run(host= "0.0.0.0", debug=True, threaded=True)
 
-    app.run(host= "0.0.0.0", debug=True, threaded=True, port=port)
